49e22b3f03c40780bdc53f1a56a1327d0e47f021
lukelowers <lalowe2@ilstu.edu>
2016-10-16 15:02:52 -0500

    Luke, Fixed conflictions

--- src/Driver.java
+++ src/Driver.java
@@ -2,11 +2,7 @@
 public class Driver {
 	public static void main(String[] args){
 		System.out.println("Hello");
-<<<<<<< HEAD
-		
 		System.out.println("Andy");
-=======
 		System.out.printn("World");
->>>>>>> 6617c758667409eefdd4750f9db7d80570ede0ad
 	}
 }
