17e25ddabf962de8627e246a8d2e24ab50148299
andrewjamen <ajamen@ilstu.edu>
2016-10-19 21:50:23 -0500

    user param

--- src/FileInput.java
+++ src/FileInput.java
@@ -210,24 +210,44 @@ public class FileInput {
 			
 			if(userParam[0] != "NONE")
 			{
-				if(userParam[0].equals(fileGradeCameFrom[0]))
+				if(userParam[0].equals(fileGradeCameFrom[0])){
+					if (students.get(i).getLetterGrade().equals("A")){
+						numOfAs++;
+					}
+					if (students.get(i).getLetterGrade().equals("B")){
+						numOfBs++;
+					}
+					if (students.get(i).getLetterGrade().equals("C")){
+						numOfCs++;
+					}
+					if (students.get(i).getLetterGrade().equals("D")){
+						numOfDs++;
+					}
+					if (students.get(i).getLetterGrade().equals("F")){
+						numOfFs++;
+					}	
+				}
+				if(userParam[2] != "NONE")
+				{
+					if(userParam[2].equals(fileGradeCameFrom[2])){
+						if (students.get(i).getLetterGrade().equals("A")){
+							numOfAs++;
+						}
+						if (students.get(i).getLetterGrade().equals("B")){
+							numOfBs++;
+						}
+						if (students.get(i).getLetterGrade().equals("C")){
+							numOfCs++;
+						}
+						if (students.get(i).getLetterGrade().equals("D")){
+							numOfDs++;
+						}
+						if (students.get(i).getLetterGrade().equals("F")){
+							numOfFs++;
+						}	
+					}
 			}
 			
-			if (students.get(i).getLetterGrade().equals("A")){
-				numOfAs++;
-			}
-			if (students.get(i).getLetterGrade().equals("B")){
-				numOfBs++;
-			}
-			if (students.get(i).getLetterGrade().equals("C")){
-				numOfCs++;
-			}
-			if (students.get(i).getLetterGrade().equals("D")){
-				numOfDs++;
-			}
-			if (students.get(i).getLetterGrade().equals("F")){
-				numOfFs++;
-			}
 		}
 		
 		gradeResults[0] = numOfAs;
